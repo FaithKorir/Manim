ab394000fc96a702a64ab50fbaea95e7594ad488
FaithKorir <166940453+FaithKorir@users.noreply.github.com>
2024-12-22 15:46:56 +0300

    Update text_to_video.py

--- text_to_video.py
+++ text_to_video.py
@@ -4,17 +4,14 @@ from manim import *
 class TextToVideo(Scene):
     def construct(self):
         text = Text("Hello Dennis, Welcome to Manim!")
-
+        
         self.play(Write(text))
         self.wait(5)
-
-        # Step 3: Transform the text into a new message
+        
         new_text = Text("Python animations made easy!")
         self.play(Transform(text, new_text))
         self.wait(5)
-        #Add sound 
+        
         self.add_sound("/Users/air/Gig/audio/hello_music.mp3")
-
-        # Step 4: Fade out the text
 
         self.play(FadeOut(text))
